7730432779a75ae87f6fd59119195484cc8246ca
jokyungho <jokh0108@gmail.com>
2020-09-12 12:36:20 +0900

    new

--- basic-data-structure/trie.py
+++ basic-data-structure/trie.py
@@ -1,70 +1,48 @@
-class Node(object):
-    def __init__(self, key, data=None):
-        self.key = key  # character
-        self.data = data  # 기존 방식에서는 True/False로 집어넣지만, 여기서는 string or None을 집어넣음.
-        self.children = {}  # 해당 char에서 다른 캐릭터로 이어지는 children character(key)들과 각 Node(value)
+import collections
 
-
-class Trie(object):
+# 트라이의 노드
+class TrieNode:
     def __init__(self):
-        self.head = Node(key=None, data=None)
+        self.word = False
+        self.children = collections.defaultdict(TrieNode)
 
-    def insert_string(self, input_string):
-        # Trie에 input_string을 넣어줌
-        cur_node = self.head
-        for c in input_string:
-            if c not in cur_node.children.keys():
-                cur_node.children[c] = Node(key=c)
-            cur_node = cur_node.children[c]
-        cur_node.data = input_string
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
 
-    def search(self, input_string):
-        # input_string이 현재 trie에 있는지 찾아서 TF를 리턴
-        cur_node = self.head
-        for c in input_string:
-            if c not in cur_node.children.keys():
+    # 단어 삽입
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.children[char]
+        node.word = True
+
+    # 단어 존재 여부 판별
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
                 return False
-            else:
-                cur_node = cur_node.children[c]
-        if cur_node.data == input_string:
-            return True
-        else:
-            return False
+            node = node.children[char]
 
-    def find_words_starts_with(self, prefix):
-        # prefix로 시작하는 모든 워드를 찾아서 리턴합니다.
-        cur_node = self.head
-        words = []
-        subtrie = None
-        for c in prefix:
-            if c in cur_node.children.keys():  # 있으므로 값을 하나씩 찾으며 내려감.
-                cur_node = cur_node.children[c]
-                subtrie = cur_node
-            else:  # prefix가 현재 trie에 없으므로, 빈 리스트를 리턴
-                return []
-        # 이제 prefix가 존재한다는 것을 알았고, subtrie에 있는 모든 워드를 찾아서 리턴하면 됨.
-        cur_nodes = [subtrie]
-        next_nodes = []
-        while True:
-            for c in cur_nodes:
-                if c.data is not None:
-                    words .append(c.data)
-                next_nodes.append(c.children.values())
-            # print("nn", [n.data for n in next_nodes])
-            if len(next_nodes) == 0:
-                break
-            else:
-                cur_nodes = next_nodes
-                next_nodes = []
-        return words
+        return node.word
 
+    # 문자열로 시작 단어 존재 여부 판별
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix: 
+            if char not in node.children:
+                return False
+            node = node.children[char]
 
-##########################################
-t = Trie()
-t.insert_string("abcd")
-t.insert_string("abdc")
-t.insert_string("acbd")
-t.insert_string("abkd")
-t.insert_string("abzzzz")
-t.search('abdc')
-t.find_words_starts_with('ab')
+        return True
+
+trie = Trie()
+print(trie.insert('apple'))
+print(trie.insert('appeal'))
+print(trie.insert('appear'))
+print(trie.search('appear'))
+print(trie.search('append'))
+print(trie.search('app'))
+print(trie.startsWith('app'))
+print(trie.startsWith('add'))
