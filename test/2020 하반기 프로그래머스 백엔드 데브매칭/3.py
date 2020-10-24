275e6886ea1f8f4add32e1d168111c4c8917db73
kyungho <jokh0108@gmail.com>
2020-10-24 13:11:15 +0900

    dev matching solutions

---
+++ test/2020 하반기 프로그래머스 백엔드 데브매칭/3.py
@@ -0,0 +1,8 @@
+def solution(n, groups):
+    answer = 0
+    return answer
+
+print(solution(10, [[1, 5], [2, 7], [4, 8], [3, 6]]), 4)
+print(solution(7, [[6, 7], [1, 4], [2, 4]]), 3)
+print(solution(100, [[1, 50], [1, 100], [51, 100]]), 1)
+
