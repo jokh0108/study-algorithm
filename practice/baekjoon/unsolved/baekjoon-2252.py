1da1340c767b652ab797867077146af5794c9973
jokh0108 <jokh0108@gmail.com>
2021-02-07 17:50:27 +0900

    new unsolved #2252

--- practice/baekjoon/unsolved/baekjoon-2252.py
+++ practice/baekjoon/unsolved/baekjoon-2252.py
@@ -2,22 +2,31 @@
 from collections import deque
 
 
-def solution(N, M, comparisons):
-    checked = set()
-    line_up = deque()
-    for a, b in comparisons:
-        if a in checked:
-            line_up.append(b)
-        elif b in checked:
-            line_up.appendleft(a)
-        else:
-            line_up.appendleft(a)
-            line_up.append(b)
+def topological_sort(graph, degrees):
+    q = deque([i for i, v in enumerate(degrees) if v == 0 and i != 0])
+    result = []
+    for _ in range(1, N + 1):
+        if not q:
+            print("cycle")
+            return
+        x = q.popleft()
+        result.append(str(x))
+        for v in graph[x]:
+            degrees[v] -= 1
+            if degrees[v] == 0:
+                q.append(v)
+    return " ".join(result)
 
-        checked |= {a, b}
-        print(checked)
-    unchecked = set(range(1, N + 1)) - checked
-    return list(unchecked) + list(line_up)
+
+def solution(N, M, comparisons):
+    graph = [[] for _ in range(N + 1)]
+    degrees = [0] * (N + 1)
+
+    for a, b in comparisons:
+        graph[a].append(b)
+        degrees[b] += 1
+
+    return topological_sort(graph, degrees)
 
 
 N, M = map(int, input().split())
